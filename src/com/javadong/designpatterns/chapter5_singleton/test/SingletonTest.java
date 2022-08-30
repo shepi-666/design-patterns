c20f920553237a659bf7cbd9a19ad1071a8baaf1
javadong <dongshaowei1201@gmail.com>
2022-08-30 17:27:36 +0800

    feat:单例模式测试用例

--- src/com/javadong/designpatterns/chapter5_singleton/test/SingletonTest.java
+++ src/com/javadong/designpatterns/chapter5_singleton/test/SingletonTest.java
@@ -1,6 +1,7 @@
 package com.javadong.designpatterns.chapter5_singleton.test;
 
 import com.javadong.designpatterns.chapter5_singleton.impl.Singleton1;
+import com.javadong.designpatterns.chapter5_singleton.impl.Singleton3;
 import org.junit.Test;
 
 /**
@@ -16,5 +17,13 @@ public class SingletonTest {
         Singleton1 instance2 = Singleton1.getInstance();
 
         System.out.println(instance1 == instance2);
+    }
+
+    @Test
+    public void testEager() {
+        Singleton3 instance1 = Singleton3.getInstance();
+        Singleton3 instance2 = Singleton3.getInstance();
+        System.out.println(instance1 == instance2);
+
     }
 }
