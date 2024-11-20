75ce2db38a275b34d710f63808ef0367c2145794
javadong <dongshaowei1201@gmail.com>
2024-11-20 21:52:55 +0800

    [add]six design principles

--- src/com/heima/designpatterns/pattern/singleton/doublecheck/Singleton.java
+++ src/com/heima/designpatterns/pattern/singleton/doublecheck/Singleton.java
@@ -1,23 +1,28 @@
-package com.heima.designpatterns.pattern.singleton.lazysafety;
+package com.heima.designpatterns.pattern.singleton.doublecheck;
 
 
 /**
  * @author: DongShaowei
  * @create: 2024-11-20 21:30
- * @description: 懒汉式：线程安全的
+ * @description: 懒汉式：双重检查锁
  */
 public class Singleton {
 
     private Singleton() { }
 
-    // 1.声明singleton类型的变量
-    private static Singleton instance;
+    // 1.声明singleton类型的变量，防止创建对象的时候进行指令重排序
+    private static volatile Singleton instance;
 
     // 2.对外提供访问方式
-    public static synchronized Singleton getInstance() {
+    public static Singleton getInstance() {
         if (instance == null) {
-            instance = new Singleton();
+            synchronized (Singleton.class) {
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+            }
         }
+        // 如果发生了指令重排序，此处返回的对象可能是半成品
         return instance;
     }
 } 
--- src/com/javadong/designpatterns/solution/Test.java
+++ src/com/javadong/designpatterns/solution/Test.java
@@ -5,10 +5,7 @@ import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println(10|12);
-        System.out.println(15&14);
-        System.out.println(2^7);
-        System.out.println(~(-15));
+        System.out.println("Team\tMP\tW\tL\tP");
 
     }
 }
